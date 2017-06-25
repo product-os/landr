01615d03c9a84a8b57a0b416d61d4b91d72761c6
craig-mulligan <craig@resin.io>
2017-06-25 20:23:06 +0100

    add debounce to get around gh-rate limits

--- packages/gatsby-source-github-repo/lib/source-node.js
+++ packages/gatsby-source-github-repo/lib/source-node.js
@@ -1,19 +1,52 @@
 const GitHubApi = require('github');
 const crypto = require('crypto');
+const _ = require('lodash')
 
 var github = new GitHubApi()
 
-module.exports = async ({ boundActionCreators }, { owner, repo }) => {
-  const { createNode } = boundActionCreators;
+module.exports = async ({ boundActionCreators, store }, { owner, repo }) => {
+  const { createNode, setPluginStatus, touchNode } = boundActionCreators;
 
-  const { data } = await github.repos.get({ owner, repo });
+  var TEN_MINS = 10 * 60000;
+  // Touch existing Github nodes so Gatsby doesn't garbage collect them.
+  touchNode(_.values(store.getState().nodes)
+    .find(n => n.internal.type === `Repo`))
 
-  const contentDigest = crypto.createHash('md5').update(JSON.stringify(data)).digest(`hex`);
+  let lastFetched
+  if (
+    store.getState().status.plugins &&
+    store.getState().status.plugins['gatsby-source-github-repo']
+  ) {
+    lastFetched = store.getState().status.plugins['gatsby-source-github-repo'].lastFetched
+  }
+  // to get around github rate limiting we only refresh data every 10 mins
+  if (lastFetched && (Date.now() - TEN_MINS < lastFetched)) {
+    console.log('*not* refetching github data');
+    return;
+  }
 
-  console.log({data})
+  let { data: repoData } = await github.repos.get({ owner, repo });
+
+  const repoMetaData = await Promise.all([
+    github.repos.getReleases({ owner, repo }),
+    github.repos.getContributors({ owner, repo })
+  ]).catch(err => {
+    console.log(err);
+  });
+
+  const allData = {
+    ...repoData,
+    releases: repoMetaData[0].data,
+    contributors: repoMetaData[1].data
+  }
+
+  setPluginStatus({ lastFetched: Date.now() })
+
+  const contentDigest = crypto.createHash('md5').update(JSON.stringify(allData)).digest(`hex`);
+
   const node = {
-    ...data,
-    id: `${data.id}`,
+    ...allData,
+    id: `${allData.id}`,
     parent: `__SOURCE__`,
     children: [],
     internal: {
