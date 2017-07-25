e125e3f758117ab0670947fe510e46d681f80a48
craig-mulligan <craig@resin.io>
2017-07-25 14:23:49 +0100

    make eject interactive

--- packages/landr/lib/eject/index.js
+++ packages/landr/lib/eject/index.js
@@ -6,32 +6,20 @@ const resinComponentsDir = path.resolve(
   `${__dirname}/../../node_modules/gatsby-plugin-landr/`
 );
 
-const ejectFile = (userDirectory, file, type) => {
+const ejectFile = (userDirectory, type, file) => {
   return fs
     .copy(
-      `${resinComponentsDir}/www/${type.name}/${file}.${type.ext}`,
-      `${userDirectory}/www/${type.name}/${file}.${type.ext}`
+      `${resinComponentsDir}/www/${type}s/${file}`,
+      `${userDirectory}/www/${type}s/${file}`
     )
     .then(() => console.log('success!'));
 };
 
+const getOpts = type => {
+  return fs.readdir(`${resinComponentsDir}/www/${type}s`);
+};
+
 module.exports = {
-  page: (userDirectory, file) => {
-    return ejectFile(userDirectory, file, {
-      name: 'pages',
-      ext: 'js'
-    });
-  },
-  component: (userDirectory, file) => {
-    return ejectFile(userDirectory, file, {
-      name: 'components',
-      ext: 'js'
-    });
-  },
-  style: userDirectory => {
-    return ejectFile(userDirectory, 'index', {
-      name: 'styles',
-      ext: 'scss'
-    });
-  }
+  ejectFile: ejectFile,
+  getOpts: getOpts
 };
