0e611cd7296d7e0069c6a79deddbdae8067be9f5
Juan Cruz Viotti <juan@balena.io>
2019-06-27 18:18:29 +0100

    Test components name properties
    Signed-off-by: Juan Cruz Viotti <juan@balena.io>

---
+++ test/components.spec.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2019 balena.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const ava = require('ava')
+const _ = require('lodash')
+const components = require('../lib/components')
+
+for (const component of _.toPairs(components)) {
+  ava(`${component[0]} should have a ${component[0]} name property`, (test) => {
+    test.is(component[0], component[1].name)
+  })
+}
