b62be1429faa48dd5bb210eb452ff1f31b5f2570
Dimitrios Lytras <dnlytras@gmail.com>
2019-06-28 18:04:49 +0300

    Integrate terminal with Jumbotron

--- lib/components/jumbotron.jsx
+++ lib/components/jumbotron.jsx
@@ -14,45 +14,46 @@
  * limitations under the License.
  */
 
-import React from 'react'
-import {
-  Box,
-  Container,
-  Txt,
-  Flex,
-  Heading
-} from 'rendition'
+import React from 'react';
+import { Box, Container, Txt, Flex, Heading } from 'rendition';
 
-export const name = 'Jumbotron'
+import Terminal from './presentational/terminal';
 
-export const variants = (metadata) => {
-  const combinations = []
+export const name = 'Jumbotron';
+
+export const variants = metadata => {
+  const combinations = [];
 
   if (metadata.data.tagline && metadata.data.description) {
     combinations.push({
       title: metadata.data.tagline,
-      description: metadata.data.description
-    })
+      description: metadata.data.description,
+      packageName: metadata.data.name,
+      type: metadata.data.type,
+    });
   }
 
   if (metadata.data.tagline) {
     combinations.push({
-      title: metadata.data.tagline
-    })
+      title: metadata.data.tagline,
+      packageName: metadata.data.name,
+      type: metadata.data.type,
+    });
   }
 
-  return combinations
-}
+  return combinations;
+};
 
-export const render = (props) => {
+export const render = props => {
   return (
     <Box bg={'#2a5070'} color="#fff" mb={5} py={6}>
       <Container>
         <Flex flexDirection="column" alignItems="center">
           <Heading.h1>{props.title}</Heading.h1>
-          {props.description && (<Txt fontSize={3}>{props.description}</Txt>)}
+          {props.description && <Txt fontSize={3}>{props.description}</Txt>}
         </Flex>
+        {props.type === 'npm' && <Terminal packageName={props.packageName} />}
       </Container>
     </Box>
-  )
-}
+  );
+};
