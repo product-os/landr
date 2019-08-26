32c87626c5f6e39da66b0d479bc40973909d1c12
Dimitrios Lytras <dnlytras@gmail.com>
2019-08-26 12:04:52 +0300

    Populate components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,14 @@
-import { configure } from '@storybook/react';
+import { addParameters, configure } from '@storybook/react';
+import { themes } from '@storybook/theming';
+
+import 'circular-std'
+import 'typeface-nunito'
+
+addParameters({
+  options: {
+    theme: themes.light,
+  },
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,43 +1,51 @@
 import React from 'react';
+import { Provider } from 'rendition';
 
 import { storiesOf } from '@storybook/react';
 
-import { render as Navigation } from '../lib/components/navigation'
-import { render as Jumbotron } from '../lib/components/jumbotron'
-import { render as Users } from '../lib/components/users'
-import { render as Contributors } from '../lib/components/contributors'
-import { render as Faq } from '../lib/components/faq'
-import { render as Footer } from '../lib/components/footer'
+import { render as Navigation } from '../lib/components/navigation';
+import { render as Jumbotron } from '../lib/components/jumbotron';
+import { render as Users } from '../lib/components/users';
+import { render as Contributors } from '../lib/components/contributors';
+import { render as Faq } from '../lib/components/faq';
+import { render as Footer } from '../lib/components/footer';
 // import { render as BlogList } from '../lib/components/blog-list'
 // import { render as DocViewer } from '../lib/components/doc-viewer'
 
-storiesOf('Navigation', module)
-  .add('Variant 1', () => <div>Lorem </div>)
-  .add('Variant 2', () => <div>Lorem </div>)
-  .add('Variant 3', () => <div>Lorem </div>)
+import {theme, props } from './config';
 
-storiesOf('Jumbotron', module)
-  .add('Variant 1', () => <div>Lorem </div>)
-  .add('Variant 2', () => <div>Lorem </div>)
-  .add('Variant 3', () => <div>Lorem </div>)
+storiesOf('Navigation', module).add('Variant 1', () => (
+  <Provider theme={theme}>
+    <Navigation {...props.Navigation} />
+  </Provider>
+));
 
-storiesOf('Users', module)
-  .add('Variant 1', () => <div>Lorem </div>)
-  .add('Variant 2', () => <div>Lorem </div>)
-  .add('Variant 3', () => <div>Lorem </div>)
+storiesOf('Jumbotron', module).add('Variant 1', () => (
+  <Provider theme={theme}>
+    <Jumbotron {...props.Jumbotron} />
+  </Provider>
+));
 
-storiesOf('Contributors', module)
-  .add('Variant 1', () => <div>Lorem </div>)
-  .add('Variant 2', () => <div>Lorem </div>)
-  .add('Variant 3', () => <div>Lorem </div>)
+storiesOf('Users', module).add('Variant 1', () => (
+  <Provider theme={theme}>
+    <Users {...props.Users} />
+  </Provider>
+));
 
-storiesOf('FAQ', module)
-  .add('Variant 1', () => <div>Lorem </div>)
-  .add('Variant 2', () => <div>Lorem </div>)
-  .add('Variant 3', () => <div>Lorem </div>)
+storiesOf('Contributors', module).add('Variant 1', () => (
+  <Provider theme={theme}>
+    <Contributors {...props.Contributing} />
+  </Provider>
+));
 
-  storiesOf('Footer', module)
-  .add('Variant 1', () => <div>Lorem </div>)
-  .add('Variant 2', () => <div>Lorem </div>)
-  .add('Variant 3', () => <div>Lorem </div>)
+storiesOf('FAQ', module).add('Variant 1', () => (
+  <Provider theme={theme}>
+    <Faq {...props.Faq} />
+  </Provider>
+));
 
+storiesOf('Footer', module).add('Variant 1', () => (
+  <Provider theme={theme}>
+    <Footer {...props.Footer} />
+  </Provider>
+));
