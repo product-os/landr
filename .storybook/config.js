da0085878abeaed565762969dece4bcb81d3e9c5
Dimitrios Lytras <dnlytras@gmail.com>
2019-08-26 13:25:28 +0300

    Minor theme touch-ups

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,21 @@
 import { addParameters, configure } from '@storybook/react';
-import { themes } from '@storybook/theming';
+import { create, themes } from '@storybook/theming';
 
 import 'circular-std'
 import 'typeface-nunito'
 import 'react-typist/dist/Typist.css'
 import '../lib/components/global.css'
+import logo from './logo.png'
+
+const theme = create({
+  brandTitle: 'Landr',
+  brandUrl: 'https://github.com/balena-io/landr',
+  brandImage: logo,
+})
 
 addParameters({
   options: {
-    theme: themes.light,
+    theme,
     showPanel: false,
   },
 });
