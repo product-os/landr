dc4df1ce2d062a0bd471c29492100735ad9e4190
Amit Solanki <amit.fash@gmail.com>
2021-10-14 19:07:33 +0530

    Update user info design
    Change-type: minor
    Signed-off-by: Amit Solanki <amit@balena.io>

---
+++ lib/components/presentational/Blockquote.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+import color from 'color'
+import {
+  Box, Txt, useTheme
+} from 'rendition'
+
+const StyledBlockQuote = styled(Box) `
+  border-left: none !important;
+  margin-top: -16px !important;
+  font-size: 24px;
+  font-stretch: normal;
+  font-style: italic;
+  line-height: 1.25;
+  letter-spacing: normal;
+  color: ${(props) => {
+    return color(props.theme.colors.primary.main).darken(0.1).hex()
+  }} !important;
+`
+
+const Quote = styled(Txt.span) `
+  line-height: 1;
+  display: inline-block;
+  vertical-align: baseline;
+  padding-right: 8px;
+  font-weight: 600;
+  font-style: normal;
+  transform: translateY(34px);
+`
+
+export const BlockQuote = ({
+  children, ...props
+}) => {
+  const theme = useTheme()
+  const darkColor = color(theme.colors.primary.main).darken(0.1).hex()
+  return (
+    <StyledBlockQuote {...props}>
+      <Quote
+        color={darkColor}
+        fontSize="80px"
+      >
+        &#8221;
+      </Quote>
+      {children}
+    </StyledBlockQuote>
+  )
+}
