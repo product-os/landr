9eab762522cdcf2605af43a2539e312e8be4aa74
Amit Solanki <amit.fash@gmail.com>
2021-10-20 16:23:42 +0000

    Remove manual basepath addition in links
    Change-type: patch
    Signed-off-by: Amit Solanki <amit@balena.io>

--- lib/components/presentational/link.jsx
+++ lib/components/presentational/link.jsx
@@ -3,19 +3,11 @@ import {
   Link as InternalRoute
 } from 'react-router-dom'
 import {
-  useBasepath
-} from 'react-static'
-import {
   Link as BaseRoute, useTheme
 } from 'rendition'
 
 const Link = (props) => {
   const theme = useTheme()
-
-  const basepath = useBasepath()
-
-  const internalUrl = `${basepath}${props.url}`
-
   return (
     <BaseRoute
       {...props}
@@ -25,7 +17,7 @@ const Link = (props) => {
         fontSize: '14px',
         fontWeight: 600
       }}
-      to={internalUrl}
+      to={props.url}
     >
       {props.children || props.text}
     </BaseRoute>
